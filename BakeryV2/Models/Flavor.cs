90c87beb3a4426d6e52e3e0d28689cb361ef4089
onurkaymak <onurkaymak@outlook.com>
2023-10-20 11:44:32 -0700

    Add validation for Name field in Flavor entity.

--- BakeryV2/Models/Flavor.cs
+++ BakeryV2/Models/Flavor.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BakeryV2.Models
 {
   public class Flavor
   {
     public int FlavorId { get; set; }
+
+    [Required(ErrorMessage = "The treat's name can't be empty!")] // validation attribute for Name.
     public string Name { get; set; }
     public List<TreatFlavor> JoinEntities { get; } // collection navigation property.
   }
