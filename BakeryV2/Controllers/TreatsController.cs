910691d730cc6692dfd20783082cdb3fe67c13a0
onurkaymak <onurkaymak@outlook.com>
2023-10-20 13:35:11 -0700

    Add DeleteJoin route in Treats Controller.

--- BakeryV2/Controllers/TreatsController.cs
+++ BakeryV2/Controllers/TreatsController.cs
@@ -117,5 +117,14 @@ namespace BakeryV2.Controllers
       return RedirectToAction("Details", new { id = treat.TreatId });
     }
 
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      TreatFlavor joinEntry = _db.TreatFlavors.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+      _db.TreatFlavors.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
