eac8466a8311822756379a508f82189a41656032
onurkaymak <onurkaymak@outlook.com>
2023-10-20 14:23:20 -0700

    Create Register GET & POST routes in Account Controller.

--- BakeryV2/Controllers/AccountController.cs
+++ BakeryV2/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using BakeryV2.Models;
 using BakeryV2.ViewModels;
+using System.Threading.Tasks;
 
 namespace BakeryV2.Controllers
 {
@@ -23,6 +24,37 @@ namespace BakeryV2.Controllers
     public ActionResult Index()
     {
       return View();
+    }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
     }
 
   }
