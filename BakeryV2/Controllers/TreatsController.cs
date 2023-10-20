272e1d901dba5945b12b029d90f24ccbe5ddc925
onurkaymak <onurkaymak@outlook.com>
2023-10-20 11:26:45 -0700

    Add Delete GET & POST routes in Treats Controller.

--- BakeryV2/Controllers/TreatsController.cs
+++ BakeryV2/Controllers/TreatsController.cs
@@ -79,5 +79,20 @@ namespace BakeryV2.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
