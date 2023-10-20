e9620a23e4c2e779daf6f9e669dfa1e1041fc0df
onurkaymak <onurkaymak@outlook.com>
2023-10-20 11:36:24 -0700

    Create Home Controller with Index route as a Splash Page.

---
+++ BakeryV2/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryV2.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BakeryV2.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ToDoListContext _db;
+    public HomeController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+
+      Treat[] treats = _db.Treats.ToArray();
+      model.Add("treats", treats);
+
+      Flavor[] flavors = _db.Flavors.ToArray();
+      model.Add("flavors", flavors);
+
+      return View(model);
+    }
+  }
+}
+
