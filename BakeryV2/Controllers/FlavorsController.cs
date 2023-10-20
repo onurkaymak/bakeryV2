56ad1ebd6060d650887649e45cabcbae6853f1f9
onurkaymak <onurkaymak@outlook.com>
2023-10-20 11:48:52 -0700

    Add Create GET & POST routes in Flavors Controller.

---
+++ BakeryV2/Controllers/FlavorsController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using BakeryV2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Collections.Generic;
+
+namespace BakeryV2.Controllers
+{
+  // [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryV2Context _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, BakeryV2Context db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(flavor);
+      }
+      else
+      {
+        _db.Flavors.Add(flavor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+  }
+}
